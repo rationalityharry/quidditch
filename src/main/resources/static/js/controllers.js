f3ef5076accd55895585ae9ded502c6ad377cfb0
Harry <miuharuma71@gmail.com>
2020-06-05 17:10:23 +0300

    ready

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -24,6 +24,7 @@ app.controller('EditUserController', ['$scope', '$http', '$location', '$routePar
     let that = this;
     let id = $routeParams.id;
     that.user = {};
+    that.id = id;
     that.image = {};
     that.isAdmin = false;
     $scope["$ctrl"] = this;
